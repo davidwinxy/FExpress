4880d2b488c42a3bed52008a5013f807147c1229
davidwinxy <daviddiaz04d@gmail.com>
2024-08-22 17:05:49 -0600

    creacion del proyecto y configuracion del layout

---
+++ src/main/java/com/FacturaExpress/FacturaExpress/FacturaExpressApplication.java
@@ -0,0 +1,13 @@
+package com.FacturaExpress.FacturaExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturaExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FacturaExpress/FacturaExpress/FacturaExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FacturaExpress.FacturaExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
